6879e9fea3526d970c74fabbed9ccb508ce9265f
Mukhtor Eshboyev <aestdile@gmail.com>
2025-03-25 07:50:09 +0500

    Add files via upload

---
+++ Bavariya.sql
@@ -0,0 +1,27 @@
+/*
+create database Bavariya;
+
+create table Footballer
+(id int,
+First_Name varchar(50),
+Last_Name varchar(50),
+Age int,
+Country varchar(50)
+);
+
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (1, 'Thomas', 'Muller', 32, 'Germany');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (2, 'Leroy', 'Sane', 28, 'Germany');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (3, 'Serge', 'Gnabry', 25, 'Germany');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (4, 'Kingle', 'Coman', 26, 'France');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (5, 'Harry', 'Kane', 33, 'England');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (6, 'Josua', 'Kimich', 27, 'Germany');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (7, 'Jamal', 'Musiala', 21, 'Germany');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (8, 'Kim', 'Min Jie', 25, 'South Korea');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (9, 'Eric', 'Dier', 28, 'England');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (10, 'Alphonso', 'Davies', 33, 'Canada');
+insert into Footballer (id, First_Name, Last_Name, Age, Country) values (11, 'Manuel', 'Neuer', 30, 'Germany');
+*/
+
+select * from Footballer;
+
+
